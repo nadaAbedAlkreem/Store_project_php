f3d126bf074bd382d35175639a086cc6f2b2d68d
nadaAbedAlkreem <elkahloutnada@gmail.com>
2022-05-06 23:42:24 +0300

    first project store

---
+++ details.php
@@ -0,0 +1,394 @@
+<?php 
+
+   include_once('connections/connection.php'); 
+      include_once('java.php'); 
+  
+   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+     if (isset($_POST['rate'])) {
+ $rate  = $_POST['rating']; 
+   $id_store  = $_POST['id'] ;   
+   $date_clicked = date('Y-m-d H:i:s');
+   $ip = getIPAddress();  
+   $check = true ; 
+      $query = "SELECT  idAddress , store_id   from ratings  ";
+       $result = mysqli_query($connection , $query) ; 
+        if(mysqli_num_rows($result) >0 ){
+         while($row = mysqli_fetch_assoc($result)){
+       if($row['idAddress'] == $ip && $row['store_id'] == $id_store){
+                      $check = false ; 
+                      break;
+                       
+          
+                  }
+      
+         }} 
+
+        
+
+ }
+ 
+
+
+
+?>
+}
+
+ 
+<!DOCTYPE HTML>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Shop &mdash; Free Website Template, Free HTML5 Template by gettemplates.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="gettemplates.co" />
+
+	<!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FreeHTML5.co
+		
+	Website: 		htt
+	p://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet"> -->
+	<!-- <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i" rel="stylesheet"> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-xs-2">
+					<div id="fh5co-logo"><a href="index.html">Stores.</a></div>
+				</div>
+				<div class="col-md-6 col-xs-6 text-center menu-1">
+					<ul>
+						<li class="has-dropdown">
+							<a href="store.php">Store</a>
+							 
+						</li>
+						<li><a href="about.php">About</a></li>
+						<li class="has-dropdown">
+						 <a href="category.php">category</a>
+							 
+						</li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+					</li>
+ 					
+				</div>
+				<div class="col-md-3 col-xs-4 text-right hidden-xs menu-2">
+					<ul>
+						<li class="search">
+							<div class="input-group">
+						      <input type="text" placeholder="Search..">
+						      <span class="input-group-btn">
+						        <button class="btn btn-primary" type="button"><i class="icon-search"></i></button>
+						      </span>
+						    </div>
+						</li>
+						 
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(images/furniture2.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>  Store </h1>
+							<h2>You can find the best stores here</h2>		 
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+	<div id="fh5co-product">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<span>Cool Stuff</span>
+					<h2>Products.</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+				</div>
+			</div>
+			<div class="row">
+								<div class="col-md-4 text-center animate-box">
+								<div class="product">
+     
+				
+						  	<?php
+     if (isset($_POST['rate'])) {
+
+if ($check) {
+
+           $query = "INSERT INTO  ratings (value , store_id , date , idAddress )
+              VALUE ('$rate' , '$id_store' , '$date_clicked' , '$ip')";
+
+              $result = mysqli_query($connection, $query) ; 
+            if($result){
+           echo   '<div class = "row " > 
+                            <div class = "col-12">
+                            <div class = "alert alert-success"> some filed are missing 
+                            </div>
+                            </div>
+                            </div>
+
+                    		' ; 
+  
+          }else {
+//    echo '<script>
+//     setTimeout(function() {
+//         swal({
+//             title: "Wow!",
+//             text: "Message!",
+//             type: "success"
+//         }, function() {
+//             window.location = "redirectURL";
+//         });
+//     }, 1000);
+// </script>';
+  echo  '<div class = "row " > 
+                            <div class = "col-12">
+                            <div class = "alert alert-danger "> some filed are missing 
+                            </div>
+                            </div>
+                            </div>
+
+                    		' ; 
+
+}
+           }
+
+       }
+
+
+
+                       // if (isset($_POST['more'])) {
+
+ 								 $id = $_POST['id'] ; 
+                            $query = "SELECT  *     from stores  where  id  = $id ";
+                           $result = mysqli_query($connection , $query) ; 
+                         if(mysqli_num_rows($result) >0 ){
+                         	while ($row = mysqli_fetch_assoc($result)) {
+                     echo
+                                  '<div class="product-grid" style="background-image:url('.'http://localhost/admin-template-master/StoreFolder/'.$row['img'].');"> </div>'.'<h3>'.$row['name'].'</h3>'
+                                  .'<h3>'.$row['address'].'</h3>'.'<h3>'.$row['mobile'].'</h3>'.'<form action = "" method ="post" id = "my">
+                  			<input type = "hidden" name = "id" value ="'.$row['id'].'">
+                  			<lable>	rating </label>
+								   <select name="rating" id="rating" class="form-control"> 
+								 	<option value="-1">  </option>
+
+								 	<option value="5"> 5 </option>
+								 	<option value="4"> 4 </option>
+								 	<option value="3"> 3 </option>
+								 	<option value="2"> 2</option>
+								 	<option value="1"> 1 </option>
+
+
+						    		</select>
+								<br>
+                   <button type ="submit" class ="btn btn-primary" id = "rate"  name ="rate" value = "rate" class= "rate-btn"> 
+                    rate now         </button></form>'
+                                   ;
+
+
+
+                          	} 
+                          }
+                      // }
+                   
+                                     		?>
+
+                  		
+ 						</div>
+					</div>
+				</div>
+			
+		</div>
+	</div>
+
+	<div id="fh5co-started">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Newsletter</h2>
+					<p>Just stay tune for our latest Product. Now you can subscribe</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2">
+					<form class="form-inline">
+						<div class="col-md-6 col-sm-6">
+					
+<html>		<div class="form-group">
+								<label for="email" class="sr-only">Email</label>
+								<input type="email" class="form-control" id="email" placeholder="Email">
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<button type="submit" class="btn btn-default btn-block">Subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-4 fh5co-widget">
+					<h3>Shop.</h3>
+					<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+				</div>
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="#">About</a></li>
+						<li><a href="#">Help</a></li>
+						<li><a href="#">Contact</a></li>
+						<li><a href="#">Terms</a></li>
+						<li><a href="#">Meetups</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="#">Shop</a></li>
+						<li><a href="#">Privacy</a></li>
+						<li><a href="#">Testimonials</a></li>
+						<li><a href="#">Handbook</a></li>
+						<li><a href="#">Held Desk</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="#">Find Designers</a></li>
+						<li><a href="#">Find Developers</a></li>
+						<li><a href="#">Teams</a></li>
+						<li><a href="#">Advertise</a></li>
+						<li><a href="#">API</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
+						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://blog.gessato.com/" target="_blank">Gessato</a> &amp; <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+<?php } 
+
+function getIPAddress() {  
+    //whether ip is from the share internet  
+     if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+                $ip = $_SERVER['HTTP_CLIENT_IP'];  
+        }  
+    //whether ip is from the proxy  
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+     }  
+//whether ip is from the remote address  
+    else{  
+             $ip = $_SERVER['REMOTE_ADDR'];  
+     }  
+     return $ip;  
+
+ }
+
+?>
---
+++ java.php
@@ -0,0 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
